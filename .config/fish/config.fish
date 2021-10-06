73660a1aba11fcc235f7e499556f1a3d5a4bddda
shinter61 <shinter61@gmail.com>
2021-10-06 19:29:05 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,10 +30,25 @@ set -x PATH $HOME/.anyenv/bin $PATH
 status --is-interactive; and source (anyenv init -|psub)
 
 # android studioの設定
-set -x ANDROID_HOME $HOME/Library/Android/sdk
-set -x PATH $PATH:$ANDROID_HOME/emulator
-set -x PATH $PATH:$ANDROID_HOME/tools
-set -x PATH $PATH:$ANDROID_HOME/tools/bin
-set -x PATH $PATH:$ANDROID_HOME/platform-tools
-set -x PATH $PATH:/Applications/"Android Studio.app"/Contents/jre/jdk/Contents/Home/bin
-set -x JAVA_HOME /Applications/"Android Studio.app"/Contents/jre/jdk/Contents/Home
+# set -x ANDROID_HOME $HOME/Library/Android/sdk
+# set -x PATH $PATH:$ANDROID_HOME/emulator
+# set -x PATH $PATH:$ANDROID_HOME/tools
+# set -x PATH $PATH:$ANDROID_HOME/tools/bin
+# set -x PATH $PATH:$ANDROID_HOME/platform-tools
+# set -x PATH $PATH:/Applications/"Android Studio.app"/Contents/jre/jdk/Contents/Home/bin
+# set -x JAVA_HOME /Applications/"Android Studio.app"/Contents/jre/jdk/Contents/Home
+
+# pyenvの設定
+pyenv init - | source
+
+# GOPATHの設定
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/shinter-pres/.pyenv/versions/anaconda3-2020.11/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+# Atcoder-Libraryの設定
+set -x CPLUS_INCLUDE_PATH ~/Custom/competitive/
